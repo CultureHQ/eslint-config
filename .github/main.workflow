1ca81d83575a943fb125039aa87819d5e015d1d4
Kevin Deisz <kevin.deisz@gmail.com>
2018-12-19 14:59:30 -0500

    Lint the lint config

--- .github/main.workflow
+++ .github/main.workflow
@@ -3,7 +3,19 @@ workflow "Main" {
   resolves = "Publish"
 }
 
+action "Install" {
+  uses = "docker://culturehq/actions-yarn:latest"
+  args = "install"
+}
+
+action "Lint" {
+  needs = "Install"
+  uses = "docker://culturehq/actions-yarn:latest"
+  args = "lint"
+}
+
 action "Tag" {
+  needs = "Lint"
   uses = "actions/bin/filter@master"
   args = "tag"
 }
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/* eslint-disable filenames/no-index */
+
 module.exports = {
   parser: "babel-eslint",
   extends: "airbnb",
@@ -11,12 +13,12 @@ module.exports = {
     "filenames/no-index": "error",
     "filenames/match-exported": "error",
     "function-paren-newline": "off",
-    "indent": ["error", 2, { "SwitchCase": 1 }],
+    indent: ["error", 2, { SwitchCase: 1 }],
     "object-curly-newline": "off",
     "object-curly-spacing": ["error", "always"],
     "quote-props": ["error", "as-needed"],
-    "quotes": ["error", "double"],
-    "react/jsx-filename-extension": ["warning", { "extensions": [".js"] }],
+    quotes: ["error", "double"],
+    "react/jsx-filename-extension": ["warning", { extensions: [".js"] }],
     "react/jsx-one-expression-per-line": "off",
     "react/no-did-mount-set-state": "off",
     "react/no-did-update-set-state": "off",

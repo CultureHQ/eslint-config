c8c9ecf3c7ac7ee7a321cb01c70fbfa2e3bcc448
Kevin Deisz <kevin.deisz@gmail.com>
2019-09-03 11:11:16 -0400

    Bump deps

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,10 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/) a
 
 ## [Unreleased]
 
+### Added
+
+- Configuration for the `jest/require-top-level-describe` rule.
+
 ## [4.0.1] - 2019-08-27
 
 ### Changed
--- rules/jest.js
+++ rules/jest.js
@@ -42,6 +42,7 @@ module.exports = {
     "jest/prefer-to-contain": "warn",
     "jest/prefer-to-have-length": "warn",
     "jest/prefer-todo": "warn",
+    "jest/require-top-level-describe": "off",
     "jest/require-tothrow-message": "off",
     "jest/valid-describe": "error",
     "jest/valid-expect": "error",
